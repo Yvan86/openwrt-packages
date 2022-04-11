a6513b693c5378a058642b2ff80a35cf6006e1f6
Yvan86 <63568944+Yvan86@users.noreply.github.com>
2022-04-11 19:48:06 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -1,3 +1,3 @@
-sudo rm -rf !(README.md|diy1.sh)
+rm -rf !(README.md|diy1.sh)
 svn export https://github.com/xiaorouji/openwrt-passwall/trunk ./passwall
 svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall ./luci-app-passwall
