999463c88a6232a4cbf7dfbd279fc991c7e6ad6a
Yvan86 <63568944+Yvan86@users.noreply.github.com>
2022-04-24 13:53:47 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -3,6 +3,6 @@ rm -rfv !(README.md|diy1.sh)
 shopt -u extglob
 git clone https://github.com/fw876/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
+git clone --branch luci --single-branch https://github.com/xiaorouji/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2
 git clone https://github.com/sirpdboy/luci-app-advanced
